516406360f68331df4738f1be747bd924eb09359
Chase <chase.ridenour@gmail.com>
2016-01-20 00:25:37 -0600

    blog is working

--- frogs/urls.py
+++ frogs/urls.py
@@ -3,12 +3,14 @@ from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.contrib import admin
 
+import frogblog.urls
 import surgery.urls
+
 
 urlpatterns = [
     # Examples:
     # url(r'^$', 'tt.views.home', name='home'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include(surgery.urls)),
-    url(r'^', include('frogblog.urls')),
+    url(r'^blog/', include('frogblog.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
